0806dceda64720a8cdbed3572ca7b72f6365adf0
wenhao <waitupon@163.com>
2017-10-13 21:01:34 +0800

    add test class

--- src/test/java/com/dwh/configruation/ApplicationContextTest.java
+++ src/test/java/com/dwh/configruation/ApplicationContextTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,9 +16,18 @@ public class ApplicationContextTest {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Autowired
+    private Environment evn;
+
     @Test
     public void testNull(){
         Assert.assertNotNull(applicationContext.getBean(User.class));
         Assert.assertNotNull(applicationContext.getBean(Runnable.class));
+    }
+
+
+    @Test
+    public void testEvn(){
+        Assert.assertEquals("127.0.0.1",evn.getProperty("server.address"));
     }
 }
